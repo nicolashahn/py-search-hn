698821cac441baa010068967d703a9fc5d3d01dd
Nicolas Hahn <nicolas@stonespring.org>
2018-01-28 11:22:13 -0800

    example output changed from # to ''' comment format

--- example.py
+++ example.py
@@ -4,19 +4,22 @@ hn = SearchHN()
 
 # Get a story, comment, or poll
 print(hn.item(1234).get())
-# Output:
-# {   'author': 'Alex3917',
-    # 'children': [   {   'author': 'pg',
-        # 'children': [   {   'author': 'Alex3917',
-# ...
+'''
+{   'author': 'Alex3917',
+    'children': [   {   'author': 'pg',
+        'children': [   {   'author': 'Alex3917',
+                            ...
+'''
 
 # Get a user
 print(hn.user('nicolashahn').get())
-# {   'about': 'Full Stack Engineer at Distribute',
-    # 'avg': 0.0,
-    # 'comment_count': 87,
-    # 'created_at': '2013-03-07T21:01:00.000Z',
-# ...
+'''
+{   'about': 'Full Stack Engineer at Distribute',
+    'avg': 0.0,
+    'comment_count': 87,
+    'created_at': '2013-03-07T21:01:00.000Z',
+    ...
+'''
 
 # String together methods to build complex queries
 from datetime import datetime
@@ -29,48 +32,52 @@ comments = (hn
 )
 for comment in comments:
     print(comment)
-# Output:
-# [{   '_highlightResult': {   'author': {   'matchLevel': 'none',
-                                          # 'matchedWords': [],
-                                          # 'value': 'nicolashahn'},
-                            # 'comment_text': {   'matchLevel': 'none',
-                                                # 'matchedWords': [],
-                                                # 'value': "I don't know about "
-                                                         # 'you or the guy '
-                                                         # 'above, but I pretty '
-                                                         # 'much always use Uber '
-                                                         # 'Pool/Lyft Line over '
-                                                         # 'the single rider '
-                                                         # ...
+'''
+[{   '_highlightResult': {   'author': {   'matchLevel': 'none',
+                                          'matchedWords': [],
+                                          'value': 'nicolashahn'},
+                            'comment_text': {   'matchLevel': 'none',
+                                                'matchedWords': [],
+                                                'value': "I don't know about "
+                                                         'you or the guy '
+                                                         'above, but I pretty '
+                                                         'much always use Uber '
+                                                         'Pool/Lyft Line over '
+                                                         'the single rider '
+                                                         ...
 
+'''
 
 # You can inspect the SearchHN object to get query parameters
 latest_btc = hn.search('bitcoin').stories().latest()
 print(latest_btc)
-# Output:
-# SearchHN object:
-# {   'base_url': 'http://hn.algolia.com/api/v1/search_by_date',
-    # ...
-    # 'param_obj': {'query': 'bitcoin', 'tags': ['story']},
-    # ...
+'''
+SearchHN object:
+{   'base_url': 'http://hn.algolia.com/api/v1/search_by_date',
+    ...
+    'param_obj': {'query': 'bitcoin', 'tags': ['story']},
+    ...
+'''
 
 # Or after execution to get the final query url
 stories = latest_btc.get(reset=False)
 print(latest_btc)
-# Output:
-# SearchHN object:
-# {   ...
-    # 'full_url': 'http://hn.algolia.com/api/v1/search_by_date?query=bitcoin&tags=story',
-    # ...
+'''
+SearchHN object:
+{   ...
+    'full_url': 'http://hn.algolia.com/api/v1/search_by_date?query=bitcoin&tags=story',
+    ...
+'''
 
 # top-level JSON fields accessible as attributes
 for story in stories: 
     print(story.title)
-# Output:
-# Blockstream CEO wants 25,000 BTC ($100M) bet over future of Bitcoin Segwit 1X/2X
-# What the world’s financial bigwigs think about Bitcoin
-# Why Bitcoin and Ethereum will soon be everywhere (for reals)
-# ...
+'''
+Blockstream CEO wants 25,000 BTC ($100M) bet over future of Bitcoin Segwit 1X/2X
+What the world’s financial bigwigs think about Bitcoin
+Why Bitcoin and Ethereum will soon be everywhere (for reals)
+...
+'''
 
 # get all comments from the latest "Who is hiring" thread:
 whoishiring = (hn
@@ -80,5 +87,6 @@ whoishiring = (hn
 python_jobs = [post for post in whoishiring if 'python' in post.comment_text.lower()]
 
 print('{} jobs available, {} python jobs available'.format(len(whoishiring), len(python_jobs)))
-# Output:
-# 70 jobs available, 45 python jobs available
+'''
+70 jobs available, 45 python jobs available
+'''
